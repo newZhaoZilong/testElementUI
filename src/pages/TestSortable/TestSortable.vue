ebbb84da3deb9af1e4138e63cf2f40ba494c4d06
shange106 <newzhaozilong@gmail.com>
2021-09-01 18:03:18 +0800

    【业务】测试sortable，失败，因为parentNode只能get不能自己set,导致自定义的target无法使用,但是应该可以设置dragEle的宽度和高度

--- src/pages/TestSortable/TestSortable.vue
+++ src/pages/TestSortable/TestSortable.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <ul id="items1">
+    <ul id="items1" class="items1">
       <li class="li-item">item 1</li>
       <li class="li-item">item 2</li>
       <li class="li-item">item 3</li>
     </ul>
-    <div id="items2">
-      <div class="div-item">item 1</div>
-      <div class="div-item">item 2</div>
-      <div class="div-item">item 3</div>
+    <div id="items2" class="items2">
+      <div class="div-item">item 4</div>
+      <div class="div-item">item 5</div>
+      <div class="div-item">item 6</div>
+      <div class="div-item">item 7</div>
+      <div class="div-item">item 8</div>
+      <div class="div-item">item 9</div>
     </div>
   </div>
 </template>
@@ -19,7 +22,7 @@ export default {
   mounted() {
     console.log("haha");
     var el1 = document.getElementById("items1");
-    new Sortable(el1, {
+    let sortable1 = new Sortable(el1, {
       //   draggable: ".item",
       sort: false,
       group: {
@@ -27,11 +30,11 @@ export default {
         put: false,
         pull: "clone",
       },
-      onAdd(e) {
-        console.log("sortables1===>onAdd", e);
+      onStart(e) {
+        console.log("onStart1", e);
       },
       //, /**Event*/ originalEvent
-      onMove: function (/**Event*/ evt) {
+      onMove: function (/**Event*/ e) {
         // Example: https://jsbin.com/nawahef/edit?js,output
         // evt.dragged; // dragged HTMLElement
         // evt.draggedRect; // DOMRect {left, top, right, bottom}
@@ -40,44 +43,57 @@ export default {
         // evt.willInsertAfter; // Boolean that is true if Sortable will insert drag element after target by default
         // originalEvent.clientY; // mouse position
         // debugger;
-        console.log("dragged", evt.dragged);
-        console.log("draggedRect", evt.draggedRect);
-        console.log("related", evt.related);
-        console.log("relatedRect", evt.relatedRect);
-        console.log("willInsertAfter", evt.willInsertAfter);
-        console.log("clientY", evt.clientY);
+        // console.log("dragged", evt.dragged);
+        // console.log("draggedRect", evt.draggedRect);
+        // console.log("related", evt.related);
+        // console.log("relatedRect", evt.relatedRect);
+        // console.log("willInsertAfter", evt.willInsertAfter);
+        // console.log("clientY", evt.clientY);
         // return false; — for cancel
         // return -1; — insert before target
         // return 1; — insert after target
-      },
-      onEnd(e) {
-        // debugger;
-        console.log("sortables1===>onEnd", e);
-      },
-    });
-    var el2 = document.getElementById("items2");
-    new Sortable(el2, {
-      group: "share",
-      onAdd(e) {
-        // debugger;
-        console.log("sortables2===>onAdd", e);
-        //, clone
-        let { target, item } = e;
         let createNodeByOld = function (old) {
           let innerText = old.innerText;
           let newNode = document.createElement("div");
-          newNode.innerText = innerText;
+          newNode.innerText = innerText + "haha";
           return newNode;
         };
         //创建一个新的节点
-        let newnode = createNodeByOld(item);
-        // target.insertBefore(newnode,clone);
-        // //通过replaceChild方法替换节点
-        target.replaceChild(newnode, item);
-        return newnode;
+        let newnode = createNodeByOld(e.dragged);
+        // debugger;
+        e.dragged = newnode;
+        console.log("onMove1", e);
+      },
+      onAdd(e) {
+        console.log("onAdd1", e);
+      },
+      onEnd(e) {
+        // debugger;
+        console.log("onEnd1", e);
+      },
+    });
+    let pre = sortable1._prepareDragStart;
+    sortable1._prepareDragStart = function (evt, touch, target) {
+      let createNodeByOld = function (old) {
+        let innerText = old.innerText;
+        let newNode = document.createElement("div");
+        newNode.innerText = "你大爷";
+        return newNode;
+      };
+      let newTarget = createNodeByOld(target);
+      newTarget.parentNode = target.parentNode
+      debugger
+      return pre.call(this, evt, touch, newTarget);
+    };
+
+    var el2 = document.getElementById("items2");
+    new Sortable(el2, {
+      group: "share",
+      onStart: function (e) {
+        console.log("onStart2", e);
       },
       //, /**Event*/ originalEvent
-      onMove: function (/**Event*/ evt) {
+      onMove: function (/**Event*/ e) {
         // debugger;
         // Example: https://jsbin.com/nawahef/edit?js,output
         // evt.dragged; // dragged HTMLElement
@@ -89,15 +105,37 @@ export default {
         // return false; — for cancel
         // return -1; — insert before target
         // return 1; — insert after target
-        console.log("dragged", evt.dragged);
-        console.log("draggedRect", evt.draggedRect);
-        console.log("related", evt.related);
-        console.log("relatedRect", evt.relatedRect);
-        console.log("willInsertAfter", evt.willInsertAfter);
-        console.log("clientY", evt.clientY);
+        // console.log("dragged", evt.dragged);
+        // console.log("draggedRect", evt.draggedRect);
+        // console.log("related", evt.related);
+        // console.log("relatedRect", evt.relatedRect);
+        // console.log("willInsertAfter", evt.willInsertAfter);
+        // console.log("clientY", evt.clientY);
+        console.log("onMove2", e);
+        e.dragged.style.width = 100;
+        e.dragged.style.height = 100;
+      },
+      onAdd(e) {
+        // debugger;
+        console.log("onAdd2", e);
+        return;
+        // //, clone
+        // let { target, item } = e;
+        // let createNodeByOld = function (old) {
+        //   let innerText = old.innerText;
+        //   let newNode = document.createElement("div");
+        //   newNode.innerText = innerText;
+        //   return newNode;
+        // };
+        // //创建一个新的节点
+        // let newnode = createNodeByOld(item);
+        // // target.insertBefore(newnode,clone);
+        // // //通过replaceChild方法替换节点
+        // target.replaceChild(newnode, item);
+        // return newnode;
       },
       onEnd(e) {
-        console.log("sortables2===>onEnd", e);
+        console.log("onEnd2", e);
       },
 
       //   draggable: ".item",
@@ -107,7 +145,14 @@ export default {
 };
 </script>
 <style scoped>
+.items2 {
+  width: 500px;
+  display: flex;
+  flex-wrap: wrap;
+}
+
 .div-item {
+  margin: 10px;
   width: 100px;
   height: 100px;
   background: red;
